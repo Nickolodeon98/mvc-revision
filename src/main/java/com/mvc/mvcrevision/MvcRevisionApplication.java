1afa4e763927cf2eb3561db09c7516596851b74a
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-08 11:19:30 +0900

    Add: MVC 패턴 복습하는 프로젝트 생성

---
+++ src/main/java/com/mvc/mvcrevision/MvcRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.mvcrevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcRevisionApplication.class, args);
+	}
+
+}
