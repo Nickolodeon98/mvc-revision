d77d536a024b7e88135fc2e204398dfd29271744
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-08 15:13:53 +0900

    feat: 지금까지 테이블에 추가된 모든 항목을 나열하는 list 페이지 생성, 기본 article 페이지에서 이곳으로 리다이렉트하도록 설정

--- src/main/java/com/mvc/mvcrevision/controller/ArticleController.java
+++ src/main/java/com/mvc/mvcrevision/controller/ArticleController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -22,6 +23,18 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
+    @GetMapping("")
+    public String index() {
+        return "redirect:/articles/list"; // 주소가 articles/list 라는 것
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "articles/list"; // 파일 위치가 articles/list 라는 것
+    }
+
     @GetMapping("/new")
     public String createPage() {
         return "articles/new";
@@ -32,7 +45,7 @@ public class ArticleController {
         log.info(articleDto.toString());
         Article savedArticle = articleRepository.save(articleDto.toEntity());
         log.info("GeneratedValue:{}", savedArticle.getId());
-        return "";
+        return String.format("redirect:/articles/%d", savedArticle.getId());
     }
 
     @GetMapping("/{id}")
@@ -43,4 +56,5 @@ public class ArticleController {
             return "articles/show";
         } else return "error";
     }
+
 }
