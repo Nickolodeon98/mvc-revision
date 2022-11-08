a09347bcc0adfd20d5540f842a9e639907666cc0
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-08 13:25:52 +0900

    feat: Added toString method in ArticleDto class, and introduced new member variable 'id' of type Long

--- src/main/java/com/mvc/mvcrevision/domain/dto/ArticleDto.java
+++ src/main/java/com/mvc/mvcrevision/domain/dto/ArticleDto.java
@@ -3,11 +3,13 @@ package com.mvc.mvcrevision.domain.dto;
 import com.mvc.mvcrevision.domain.entity.Article;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public class ArticleDto {
-//    private int id;
+    private Long id;
     private String title;
     private String contents;
 
